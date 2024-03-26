817357898f09c0620d9ba6d9dfc590c708c517a0
alexmf91 <alexmunoz91@gmail.com>
2024-03-26 18:32:35 +0100

    refactor: Update Header component styling

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,10 +3,10 @@ import { ConnectWalletButton } from './ui'
 export default function Header() {
   return (
     <header className="p-8 flex justify-center items-center">
-      <div className="border border-gray-600 w-1/2 p-2 rounded-full flex items-center justify-between">
+      <div className="border border-gray-600 lg:w-1/2 p-2 rounded-full flex items-center justify-between gap-10">
         <div className="flex items-center gap-4">
           <img src="/ethereum-logo.svg" alt="eth-logo" width={40} />
-          <h1 className="text-gray-100 text-2xl">ERC20 Wallet Watcher</h1>
+          <h1 className="text-gray-100 text-xl xl:text-2xl hidden lg:flex">ERC20 Wallet Watcher</h1>
         </div>
         <ConnectWalletButton />
       </div>
