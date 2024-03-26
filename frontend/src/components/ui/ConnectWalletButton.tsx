e08aa437fb3865f888932ec376ddba172068d748
alexmf91 <alexmunoz91@gmail.com>
2024-03-26 18:11:04 +0100

    feat: Add ConnectWalletButton component

---
+++ frontend/src/components/ui/ConnectWalletButton.tsx
@@ -0,0 +1,27 @@
+import { formatAddress } from '@/utils'
+import { ConnectKitButton } from 'connectkit'
+import { twMerge } from 'tailwind-merge'
+
+type Props = {
+  className?: string
+}
+
+export default function ConnectWalletButton({ className }: Props) {
+  return (
+    <ConnectKitButton.Custom>
+      {({ isConnected, show, address }) => {
+        return (
+          <button
+            onClick={show}
+            className={twMerge(
+              'text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-full text-sm px-5 py-2.5 text-center',
+              className
+            )}
+          >
+            {isConnected ? formatAddress(address) : 'Connect Wallet'}
+          </button>
+        )
+      }}
+    </ConnectKitButton.Custom>
+  )
+}
