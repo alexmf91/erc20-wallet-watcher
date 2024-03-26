999a281c3c763cce49ebaf52796237e25f82c90f
alexmf91 <alexmunoz91@gmail.com>
2024-03-26 18:10:15 +0100

    feat: Add Web3Provider configuration

---
+++ frontend/src/config/Web3Provider.tsx
@@ -0,0 +1,28 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ConnectKitProvider, getDefaultConfig } from 'connectkit'
+import { WagmiProvider, createConfig, http } from 'wagmi'
+import { sepolia } from 'wagmi/chains'
+
+const config = createConfig(
+  getDefaultConfig({
+    walletConnectProjectId: import.meta.env.VITE_WALLETCONNECT_PROJECT_ID,
+    chains: [sepolia],
+    transports: {
+      [sepolia.id]: http(import.meta.env.VITE_RPC_PROVIDER_URL)
+    },
+    appName: 'Erc20 Wallet Watcher',
+    appDescription: 'Track your ERC20 tokens with ease.'
+  })
+)
+
+const queryClient = new QueryClient()
+
+export const Web3Provider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <ConnectKitProvider>{children}</ConnectKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  )
+}
