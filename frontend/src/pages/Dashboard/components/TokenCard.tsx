44f9692c80f313be7dd22faad70cfab941fe12af
alexmf91 <alexmunoz91@gmail.com>
2024-03-27 18:51:42 +0100

    fix: Update TokenCard padding x

--- frontend/src/pages/Dashboard/components/TokenCard.tsx
+++ frontend/src/pages/Dashboard/components/TokenCard.tsx
@@ -9,11 +9,11 @@ type Props = {
 
 export default function TokenCard({ token, onFetchData, address }: Props) {
   return (
-    <li className="bg-indigo-400/10  px-6 py-8 rounded-lg flex justify-between items-center flex-col gap-6 w-fit lg:flex-row lg:w-full">
+    <li className="bg-indigo-400/10 px-4 sm:px-6 py-8 rounded-lg flex justify-between items-center flex-col gap-6 w-fit lg:flex-row lg:w-full">
       <h3 className="text-xl font-semibold text-gray-200">
         {token.name} ({token.symbol})
       </h3>
-      <div className="space-x-4">
+      <div className="space-x-2 sm:space-x-4">
         <Button
           className="px-4 py-2 rounded"
           color="indigo"
