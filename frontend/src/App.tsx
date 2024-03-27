971e8d9758c02d115fc5caba41627dd14dde9f10
alexmf91 <alexmunoz91@gmail.com>
2024-03-27 18:37:42 +0100

    feat: Add routes and guards to App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
+import { Route, Routes } from 'react-router-dom'
+import { PrivateRoutesGuard } from './components/guards'
 import { AppLayout } from './components/ui/layouts'
-import { Home } from './pages'
+import { Dashboard, Home, NotFound } from './pages'
+import { PrivateRoutes, PublicRoutes } from './utils/routes'
 
 export default function App() {
   return (
     <AppLayout>
-      <Home />
+      <Routes>
+        <Route path={PublicRoutes.HOME} element={<Home />} />
+        <Route path={PublicRoutes.NOT_FOUND} element={<NotFound />} />
+        <Route element={<PrivateRoutesGuard />}>
+          <Route path={PrivateRoutes.DASHBOARD} element={<Dashboard />} />
+        </Route>
+      </Routes>
     </AppLayout>
   )
 }
