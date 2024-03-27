6f7c0720ccd6c56b7ddd4a2e5ee1fe7d1eb27a16
alexmf91 <alexmunoz91@gmail.com>
2024-03-27 18:10:04 +0100

    feat: Add BrowserRouter to main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import { Web3Provider } from './config'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Web3Provider>
-      <App />
-    </Web3Provider>
+    <BrowserRouter>
+      <Web3Provider>
+        <App />
+      </Web3Provider>
+    </BrowserRouter>
   </React.StrictMode>
 )
