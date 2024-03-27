b444fb94d801d1aa73549d226f760a23d0c9c73a
alexmf91 <alexmunoz91@gmail.com>
2024-03-27 18:11:46 +0100

    refactor: Update Home page with ConnectWalletButton and redirect logic

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,8 +1,17 @@
-import { ConnectWalletButton } from '@/components/ui'
+import { ConnectWalletButton } from '@/components/ui/buttons'
+import { PrivateRoutes } from '@/utils/routes'
+import { Navigate } from 'react-router-dom'
+import { useAccount } from 'wagmi'
 
 export default function Home() {
+  const { address } = useAccount()
+
+  if (address) {
+    return <Navigate to={PrivateRoutes.DASHBOARD} replace />
+  }
+
   return (
-    <section className="grid place-items-center gap-12">
+    <section className="grid place-items-center gap-12 py-20 lg:py-40">
       <h2 className="text-7xl text-gray-100 font-medium">Empower Your Crypto Experience</h2>
       <p className="text-gray-500 text-xl">Effortlessly Connect, Analyze, and Optimize Your Digital Assets.</p>
 
