8968b4521cdb7d521dc7757f96626a7f6ffa6d11
alexmf91 <alexmunoz91@gmail.com>
2024-03-27 18:16:34 +0100

    feat: Add TokenCard Dashboard component

---
+++ frontend/src/pages/Dashboard/components/TokenCard.tsx
@@ -0,0 +1,41 @@
+import { Button } from '@/components/ui/buttons'
+import { CoinsApiResource, type CoinsApiUrlSegment } from '../types'
+
+type Props = {
+  token: { name: string; symbol: string; path: CoinsApiUrlSegment }
+  address: string
+  onFetchData: (path: CoinsApiUrlSegment, resource: CoinsApiResource, address?: string) => void
+}
+
+export default function TokenCard({ token, onFetchData, address }: Props) {
+  return (
+    <li className="bg-indigo-400/10  px-6 py-8 rounded-lg flex justify-between items-center flex-col gap-6 w-fit lg:flex-row lg:w-full">
+      <h3 className="text-xl font-semibold text-gray-200">
+        {token.name} ({token.symbol})
+      </h3>
+      <div className="space-x-4">
+        <Button
+          className="px-4 py-2 rounded"
+          color="indigo"
+          onClick={() => onFetchData(token.path, CoinsApiResource.Details)}
+        >
+          Details
+        </Button>
+        <Button
+          className="px-4 py-2 rounded"
+          color="indigo"
+          onClick={() => onFetchData(token.path, CoinsApiResource.TotalSupply)}
+        >
+          Total Supply
+        </Button>
+        <Button
+          className="px-4 py-2 rounded"
+          color="indigo"
+          onClick={() => onFetchData(token.path, CoinsApiResource.Balance, address)}
+        >
+          Balance
+        </Button>
+      </div>
+    </li>
+  )
+}
