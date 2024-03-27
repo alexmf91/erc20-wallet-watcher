b073123d203c65378cd48dd172e36a2c278f627d
alexmf91 <alexmunoz91@gmail.com>
2024-03-27 18:05:21 +0100

    feat: add address formatting and add case string formatting

--- frontend/src/utils/address.ts
+++
@@ -1,6 +0,0 @@
-export const formatAddress = (address: string | undefined): string => {
-  if (address && address.length > 9) {
-    return `${address.substring(0, 5)}....${address.substring(address.length - 4)}`
-  }
-  return address ?? ''
-}
--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -1,1 +1,10 @@
-export * from './address';
+export const formatAddress = (address: string | undefined): string => {
+  if (address && address.length > 9) {
+    return `${address.substring(0, 5)}....${address.substring(address.length - 4)}`
+  }
+  return address ?? ''
+}
+
+export const formatCaseString = (str: string): string => {
+  return str.replace(/-/g, ' ').replace(/([a-z])([A-Z])/g, '$1 $2')
+}
